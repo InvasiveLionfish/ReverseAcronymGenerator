23f8d7efcd42c084fd4be3fe4835882628dfb373
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-08-26 13:42:15 -0700

    Added Unit Tests

---
+++ starting/src/test/java/hello/QuoteTest.java
@@ -0,0 +1,34 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class QuoteTest {
+    @Test
+    public void getType() throws Exception {
+
+    }
+
+    @Test
+    public void setType() throws Exception {
+
+    }
+
+    @Test
+    public void getValue() throws Exception {
+
+    }
+
+    @Test
+    public void setValue() throws Exception {
+
+    }
+
+    @Test
+    public void toString() throws Exception {
+
+    }
+
+}
