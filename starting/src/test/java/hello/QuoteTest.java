8d650e626fe42eed5ed78cf555085faa92e48e8f
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-08-26 11:58:15 -0700

    Test folder structure

---
+++ starting/src/test/java/hello/QuoteTest.java
@@ -0,0 +1,9 @@
+package hello;
+
+import main.java.hello;
+import net.codestory.simplelenium.SeleniumTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
